c7b62fb295b60d2ac5e9b5773a821f341ceb2bf3
Lucas Santana Lellis <lucaslellis777@gmail.com>
2020-10-23 10:35:46 -0300

    hapter-3-the-repository

--- rentomatic/repository/memrepo.py
+++ rentomatic/repository/memrepo.py
@@ -5,5 +5,22 @@ class MemRepo:
     def __init__(self, data):
         self.data = data
 
-    def list(self):
-        return [r.Room.from_dict(i) for i in self.data]
+    def list(self, filters=None):
+        result = [r.Room.from_dict(i) for i in self.data]
+
+        if filters is None:
+            return result
+
+        if 'code__eq' in filters:
+            result = [room for room in result if room.code == filters['code__eq']]
+
+        if 'price__eq' in filters:
+            result = [room for room in result if room.price == int(filters['price__eq'])]
+
+        if 'price__lt' in filters:
+            result = [room for room in result if room.price < int(filters['price__lt'])]
+
+        if 'price__gt' in filters:
+            result = [room for room in result if room.price > int(filters['price__gt'])]
+
+        return result
