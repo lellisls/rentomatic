4d6fd7f5be36ef7c09940c4009c17510fdb5921d
Lucas Santana Lellis <lucaslellis777@gmail.com>
2020-10-23 10:27:13 -0300

    chapter-3-the-http-server

--- rentomatic/rest/room.py
+++ rentomatic/rest/room.py
@@ -1,12 +1,22 @@
 import json
 
-from flask import Blueprint, Response
+from flask import Blueprint, request, Response
 
 from rentomatic.repository import memrepo as mr
+from rentomatic.request_objects.room_list_request_object import RoomListRequestObject
+from rentomatic.response_objects.response_objects import ResponseSuccess, ResponseFailure
 from rentomatic.serializers import room_json_serializer as ser
 from rentomatic.use_cases import room_list_use_case as uc
 
 blueprint = Blueprint('room', __name__)
+
+STATUS_CODES = {
+    ResponseSuccess.SUCCESS: 200,
+    ResponseFailure.RESOURCE_ERROR: 404,
+    ResponseFailure.PARAMETERS_ERROR: 400,
+    ResponseFailure.SYSTEM_ERROR: 500
+}
+
 room1 = {
     'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35a',
     'size': 215,
@@ -34,6 +44,17 @@ room3 = {
 def room():
     repo = mr.MemRepo([room1, room2, room3])
     use_case = uc.RoomListUseCase(repo)
-    result = use_case.execute()
-    return Response(json.dumps(result, cls=ser.RoomJsonEncoder),
-                    mimetype='application/json', status=200)
+
+    query_str_params = {
+        'filters': {},
+    }
+
+    for arg, values in request.args.items():
+        if arg.startswith('filter_'):
+            query_str_params['filters'][arg.replace('filter_', '')] = values
+
+    request_object = RoomListRequestObject.from_dict(query_str_params)
+    response = use_case.execute(request_object)
+
+    return Response(json.dumps(response.value, cls=ser.RoomJsonEncoder),
+                    mimetype='application/json', status=STATUS_CODES[response.type])
